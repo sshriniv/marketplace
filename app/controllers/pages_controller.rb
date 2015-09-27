bf554156a81a2a0804fa6af119449a2f010259ed
Sandeep Shrinivasan <sandeep.shri@gmail.com>
2015-09-27 13:20:09 -0700

    Created Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:dashboard]
+
   def home
   	if current_user
   		redirect_to products_path
   	end
+  end
+
+  def dashboard
+  	@products = Product.all
   end
 end
